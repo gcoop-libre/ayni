ec064aa7f33e9e97dce03d48966b306a9526eab2
Hugo <hugo@gcoop.com.ar>
2010-03-01 17:46:53 -0300

    implementando sistema de sonidos basico

---
+++ audio.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+# Ayni
+#
+# Copyright 2009 - Gcoop <info@gcoop.coop>
+# License: GPLv3 (see http://www.gnu.org/licenses/gpl.html)
+
+import pygame
+
+ENABLE_SOUND = True
+
+class Audio:
+    "Representa el sistema de sonidos."
+
+    def __init__(self):
+        if ENABLE_SOUND:
+            pygame.mixer.init()
+
+            self.sounds = {
+                'working': pygame.mixer.Sound('data/sounds/working.wav'),
+                }
+
+    def play(self, name):
+        if ENABLE_SOUND:
+            self.sounds[name].play()
